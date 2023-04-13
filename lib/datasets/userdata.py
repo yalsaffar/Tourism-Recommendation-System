c49577dde53165a65f0202417fd4950e7a15c35f
yalsaffar <yalsaffar.ieu2020@student.ie.edu>
2023-04-14 00:46:40 +0200

    fixes

--- lib/datasets/userdata.py
+++ lib/datasets/userdata.py
@@ -6,57 +6,40 @@ import pandas as pd
 def users(userid ,df, user_df, pool_df, city, cat, price, time, indicator):
     #global userid 
     #print(df, user_df, pool_df, city, cat, price, time, indicator)
-    user_data = user_df
+    #user_data = user_df
+    #print('/////////////////////',indicator)
 
-    def like_price_city_cat_time(userid, city, cat, price, time):
+    def like_price_city_cat_time(userid, city, cat, price, time, user_df):
         time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-        user_df.loc[(user_df['UserId']==userid),city] += 1
-        user_df.loc[(user_df['UserId']==userid),cat] += 1
-        if time.hour >= 4 and time.hour < 7:
-            user_df.loc[(user_df['UserId']==userid), 'EarlyMorning'] +=1
-        if time.hour >= 7 and time.hour < 12:
-            user_df.loc[(user_df['UserId']==userid), 'Morning'] +=1
-        if time.hour >= 12 and time.hour < 16:
-            user_df.loc[(user_df['UserId']==userid), 'Noon'] +=1
-        if time.hour >= 16 and time.hour < 19:
-            user_df.loc[(user_df['UserId']==userid), 'Afternoon'] +=1
-        if time.hour >= 19 and time.hour < 23:
-            user_df.loc[(user_df['UserId']==userid), 'Evening'] +=1
-        if time.hour >= 24 and time.hour < 4:
-            user_df.loc[(user_df['UserId']==userid), 'Night'] +=1
-        if price < 100:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_1'] += 1
-        elif price < 200 and price >= 100:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_2'] += 1
-        elif price < 300 and price >= 200:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_3'] += 1
-        elif price < 400 and price >= 300:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_4'] += 1
-        elif price < 500 and price >= 400:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_5'] += 1
-        elif price < 600 and price >= 500:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_6'] += 1
-        elif price < 700 and price >= 600:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_7'] += 1
-        elif price < 800 and price >= 700:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_8'] += 1
-        elif price < 900 and price >= 800:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_9'] += 1
-        elif price < 1000 and price >= 900:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_10'] += 1
-        elif price < 1500 and price >= 1000:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_15'] += 1
-        elif price < 2000 and price >= 1500:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_20'] += 1
-        elif price < 3000 and price >= 2000:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_30'] += 1
-        elif price < 4000 and price >= 3000:
-            user_df.loc[(user_df['UserId']==userid),'PriceLvl_40'] += 1
+        user_df.loc[user_df['UserId'] == userid, city] += 1
+        user_df.loc[user_df['UserId'] == userid, cat] += 1
+        if 4 <= time.hour < 7:
+            user_df.loc[user_df['UserId'] == userid, 'EarlyMorning'] += 1
+        elif 7 <= time.hour < 12:
+            user_df.loc[user_df['UserId'] == userid, 'Morning'] += 1
+        elif 12 <= time.hour < 16:
+            user_df.loc[user_df['UserId'] == userid, 'Noon'] += 1
+        elif 16 <= time.hour < 19:
+            user_df.loc[user_df['UserId'] == userid, 'Afternoon'] += 1
+        elif 19 <= time.hour < 23:
+            user_df.loc[user_df['UserId'] == userid, 'Evening'] += 1
+        else:
+            user_df.loc[user_df['UserId'] == userid, 'Night'] += 1
+
+        price_levels = {100: 'PriceLvl_1', 200: 'PriceLvl_2', 300: 'PriceLvl_3', 400: 'PriceLvl_4', 500: 'PriceLvl_5',
+                        600: 'PriceLvl_6', 700: 'PriceLvl_7', 800: 'PriceLvl_8', 900: 'PriceLvl_9', 1000: 'PriceLvl_10',
+                        1500: 'PriceLvl_15', 2000: 'PriceLvl_20', 3000: 'PriceLvl_30', 4000: 'PriceLvl_40'}
+
+        for i in range(1, 21):
+            if price < i * 100:
+                user_df.loc[user_df['UserId']==userid, f'PriceLvl_{i}'] += 1
+                break
+
         return user_df
 
 
 
-    def dislike_price_city_cat_time(userid, city, cat, price, time):
+    def dislike_price_city_cat_time(userid, city, cat, price, time, user_df):
         time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
         user_df.loc[(user_df['UserId']==userid), city] -= 1
         user_df.loc[(user_df['UserId']==userid), cat] -= 1
@@ -107,6 +90,7 @@ def users(userid ,df, user_df, pool_df, city, cat, price, time, indicator):
 
     def record_interaction(userid, city, cat, price, time, indicator,pool_df):
         #global pool_df
+        
         new_row = pd.DataFrame({col: [0] for col in pool_df.columns})
         new_row[shared_cols] = user_df.loc[(user_df['UserId']==userid)][shared_cols].values
         time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
@@ -121,10 +105,10 @@ def users(userid ,df, user_df, pool_df, city, cat, price, time, indicator):
         pool_df = pool_df.append(new_row, ignore_index=True)
         return pool_df
     records = record_interaction(userid, city, cat, price, time, indicator,pool_df)
+    data_temp = None
     if indicator == 0:
-        user_data = dislike_price_city_cat_time(userid, city, cat, price, time)
+        data_temp = dislike_price_city_cat_time(userid, city, cat, price, time, user_df)
     elif indicator == 1:
-        user_data = like_price_city_cat_time(userid, city, cat, price, time)
+        data_temp = like_price_city_cat_time(userid, city, cat, price, time, user_df)
     #print(user_data)
-    return records, user_data
-
+    return records, data_temp
--- lib/models/model.py
+++ lib/models/model.py
@@ -15,14 +15,14 @@ warnings.filterwarnings("ignore")
 
 
 #titles,indices, cosine_sim, df = model_computations()
-def clf(counter , cosine_sim,user_df, pool_df,previous='ALC'):
+def clf( user_df, pool_df,cosine_sim,previous='ALC'):
     #titles,indices, cosine_sim, df = model_computations()
     titles = pd.read_csv('../datasets/data_files/titles.csv')
     df = pd.read_csv('../datasets/data_files/df.csv')
     df2 = df.reset_index()
     titles = pd.Series(df2['Description'])
     indices = pd.Series(df2.index, index = df2['Description'])
-    cosine_sim = np.load('../datasets/data_files/cosine_sim.npy')
+    #cosine_sim = np.load('../datasets/data_files/cosine_sim.npy')
     
     #_, user_df, pool_df = data_strcture()
 
@@ -36,26 +36,21 @@ def clf(counter , cosine_sim,user_df, pool_df,previous='ALC'):
     
 
     
-    
+    print(user_df)
     location = input()
 
-    city_names = list(set(df['cityTo'])) # Replace with your desired cities
+    city_names = list(set(df['cityTo'])) 
     city_counts = user_df.filter(items=city_names).sum().sort_values(ascending=False)
 
     highest_city = city_counts.index[0]
-    second_highest_city = city_counts.index[1]
-    third_highest_city = city_counts.index[2]
+    #second_highest_city = city_counts.index[1]
+    #third_highest_city = city_counts.index[2]
 
     price_max_col = user_df.filter(like='PriceLvl_').idxmax(axis=1)
     activity_max_col = user_df.filter(['Beach', 'Nature', 'Cultural', 'Historical', 'Adventurous']).idxmax(axis=1)
     time_max_col = user_df.filter(['EarlyMorning', 'Morning', 'Noon', 'Afternoon', 'Evening', 'Night']).idxmax(axis=1)
     
-    if counter != 3:
-        reco = Flights_Reco(location,previous,cities_dict[highest_city])
-    elif counter == 6:
-        reco = Flights_Reco(location,previous,cities_dict[third_highest_city])
-    else:
-        reco = Flights_Reco(location,previous,cities_dict[second_highest_city])
+    reco = Flights_Reco(location,previous,cities_dict[highest_city])
     reco['local_departure'] = pd.to_datetime(reco['local_departure'])
 
     reco_filtered = reco[(reco['price'] >= price_ranges[str(price_max_col[0])][0]) & (reco['price'] <= price_ranges[str(price_max_col[0])][1])]
@@ -70,11 +65,11 @@ def clf(counter , cosine_sim,user_df, pool_df,previous='ALC'):
     reco_final = reco_final.drop_duplicates(keep='first')
 
     previous = reco_final['flyTo'].iloc[0]
-
-    print(reco_final.head(1))
+    print("-------------------------------")
     print("Do you like this offer: (yes:1, no:0)")
     indicator = input()
+    #print( reco_final['cityTo'].iloc[0], reco_final['category'].iloc[0], reco_final['price'].iloc[0], str(reco_final['local_departure'].iloc[0]), indicator)
     
-    
-    pool_df, user_df = users(0 ,df, user_df, pool_df, reco_final['cityTo'].iloc[0], reco_final['category'].iloc[0], reco_final['price'].iloc[0], str(reco_final['local_departure'].iloc[0]), indicator)
+    pool_df, updated_user_df = users(0 ,df, user_df, pool_df, reco_final['cityTo'].iloc[0], reco_final['category'].iloc[0], reco_final['price'].iloc[0], str(reco_final['local_departure'].iloc[0]), int(indicator))
+    user_df = updated_user_df.copy()
     return pool_df, user_df 
