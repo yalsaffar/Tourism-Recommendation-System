87f03b003c9263dee39d5511afd7d4b38109c6da
yalsaffar <yalsaffar.ieu2020@student.ie.edu>
2023-04-19 20:18:13 +0200

    changes

--- lib/models/main.py
+++ lib/models/main.py
@@ -3,14 +3,17 @@ import pandas as pd
 import numpy as np
 from userdata import users
 def main():
+    #location
+    #userd
     user_df = pd.read_csv('../datasets/data_files/user_df.csv')
     pool_df = pd.read_csv('../datasets/data_files/pool_df.csv')
     df_air = pd.read_csv('../datasets/data_files/airbnb_clean.csv')
     df_flight = pd.read_csv('../datasets/data_files/df.csv')
     cosine_sim = np.load('../datasets/data_files/cosine_sim.npy')
+
     Reco_1, City, Category, Price_flight, time = clf( user_df, pool_df,cosine_sim,location,previous='ALC')
     # More cities are here, need to be fixed
-    pool_df, user_df, Reco_2, City, Category, Price_flight, time, Price_Air,Beds_air,People_air,Reviews_air = clf_Airbnb(1,user_df,pool_df,df_air,City,Price_flight,Category,time,prev = 50059918)
+    pool_df, user_df, Reco_2, City, Category, Price_flight, time, Price_Air,Beds_air,People_air,Reviews_air = clf_Airbnb(UserId,user_df,pool_df,df_air,City,Price_flight,Category,time,prev = 50059918)
     
     pool_df, updated_user_df = users(UserId ,df_air, user_df, pool_df, City, Category, Price_flight,Price_Air,Beds_air,People_air,Reviews_air, time, int(indicator))
     user_df = updated_user_df.copy()
